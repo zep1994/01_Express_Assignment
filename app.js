1640bd05a18fe948e685554cb09448fd49bdfda2
zep1994 <snaxbags@protonmail.com>
2019-10-23 22:52:27 -0400

    udpate app.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 //const routes = require('./routes/routes')
+
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-game', (req, res, next) => {
     res.send("<form action='/product' method='POST'><input type='text' name='title'/><button='submit'>Submit</submit></form>")
 })
 
+//GET
 app.use('/product', (req, res, next) => {
     //redirect and log incoming data
     res.redirect('/')
